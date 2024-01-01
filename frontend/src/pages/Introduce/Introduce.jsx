d68bf2f1d160d6205430f0a540fbc4ee9dd2430a
Hoa0304 <tranthicamhoa2k4@gmail.com>
2024-01-02 01:29:21 +0700

    do an co so 2

---
+++ frontend/src/pages/Introduce/Introduce.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import style from './Introduce.module.scss'
+
+const Introduce = () => {
+	return (
+		<section className={style.introduce}>
+			<div className={style.wrapper}>
+				<h1 className={style.heading}>Giới thiệu</h1>
+				<p className={style.desc}>
+					Chào mừng đến với trang web của chúng tôi, nơi bạn có thể tìm thấy các sản phẩm công nghệ hàng đầu và đa dạng. Chúng tôi tự hào là một nền tảng mua sắm trực tuyến đáng tin cậy, mang đến cho bạn trải nghiệm mua sắm thuận tiện và hài lòng.Với chất lượng và độ tin cậy là ưu tiên hàng đầu, chúng tôi cung cấp một loạt các sản phẩm công nghệ từ các thương hiệu hàng đầu trên thế giới. Tại đây, bạn có thể tìm thấy điện thoại di động, máy tính xách tay, máy tính bảng, máy ảnh, âm thanh và video, phụ kiện công nghệ, và nhiều sản phẩm khác.
+					Chúng tôi cam kết đảm bảo rằng mọi sản phẩm trên trang web đều được chọn lọc kỹ lưỡng và được kiểm tra chất lượng trước khi đến tay bạn. Chúng tôi luôn cập nhật thông tin sản phẩm và đảm bảo giá cả cạnh tranh, để bạn có thể an tâm mua sắm mà không lo lắng về chất lượng và giá trị.
+					Trang web của chúng tôi cung cấp một giao diện dễ sử dụng, cho phép bạn duyệt qua các danh mục sản phẩm, tìm kiếm theo từ khóa cụ thể, và đặt hàng một cách nhanh chóng và dễ dàng. Chúng tôi cũng cam kết đảm bảo an toàn và bảo mật thông tin cá nhân của bạn trong quá trình giao dịch.
+					Hãy khám phá trang web của chúng tôi và tận hưởng trải nghiệm mua sắm công nghệ tuyệt vời. Nếu bạn có bất kỳ câu hỏi nào, đừng ngần ngại liên hệ với đội ngũ hỗ trợ của chúng tôi. Chúng tôi sẵn lòng giúp đỡ và đảm bảo bạn có trải nghiệm mua sắm tốt nhất có thể. Cảm ơn bạn đã ghé thăm trang web của chúng tôi!
+					Nếu bạn đang tìm kiếm một trang web để mua và bán hàng trực tuyến thì
+					MCart là một sự lựa chọn tuyệt vời dành cho bạn. MCart là trang thương
+					mại điện tử cho phép người mua và người bán tương tác và trao đổi dễ
+					dàng thông tin về sản phẩm và chương trình khuyến mãi của shop. Do đó,
+					việc mua bán trên MCart trở nên nhanh chóng và đơn giản hơn. Bạn có
+					thể trò chuyện trực tiếp với nhà bán hàng để hỏi trực tiếp về mặt hàng
+					cần mua. Để bạn có thể dễ dàng khi tìm hiểu và sử dụng sản phẩm, MCart
+					Blog - trang blog thông tin chính thức của MCart - sẽ giúp bạn có thể
+					tìm được cho mình các kiến thức về xu hướng thời trang, review công
+					nghệ, mẹo làm đẹp, tin tức tiêu dùng và deal giá tốt bất ngờ. Đến với
+					MCart, cơ hội để trở thành một nhà bán hàng dễ dàng hơn bao giờ hết.
+					Chỉ với vài thao tác trên ứng dụng, bạn đã có thể đăng bán ngay những
+					sản phẩm của mình. Không những thế, các nhà bán hàng có thể tự tạo
+					chương trình khuyến mãi trên MCart để thu hút người mua với những sản
+					phẩm có mức giá hấp dẫn. Khi đăng nhập tại ND Mall Kênh người bán, bạn
+					có thể dễ dàng phân loại sản phẩm, theo dõi đơn hàng, chăm sóc khách
+					hàng và cập nhập ngay các hoạt động của shop.
+				</p>
+			</div>
+		</section>
+	)
+}
+
+export default Introduce
